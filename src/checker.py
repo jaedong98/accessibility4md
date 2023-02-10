e3c639006f6608a714d27277f35b73b596d325bc
Jae Dong Hwang <jaedonghwang@microsoft.com>
2023-02-10 05:40:31 -0800

    TESTED: SimpleTagPattern

--- src/checker.py
+++ src/checker.py
@@ -1,6 +1,6 @@
 import markdown
 from myextension import MyExtension
-print(markdown.markdown('foo bar', extensions=[MyExtension()]))
+print(markdown.markdown('foo --deleted-- bar', extensions=[MyExtension()]))
 
-#html = markdown.markdown("# Hello World!\n NO RENDER", extensions=[NoRender()])
+#html = markdown.markdown("# Hello World!", extensions=[MyExtension()])
 #print(html)
--- src/myextension/myextension.py
+++ src/myextension/myextension.py
@@ -1,6 +1,12 @@
 from markdown.extensions import Extension
+from markdown.inlinepatterns import SimpleTagPattern
 
+DEL_RE = r'(--)(.*?)--'
+    
 class MyExtension(Extension):
-   def extendMarkdown(self, md):
-       # Insert code here to change markdown's behavior.
-       pass
+    def extendMarkdown(self, md):
+        # Create the del pattern
+        del_tag = SimpleTagPattern(DEL_RE, 'del')
+        # Insert del pattern into markdown parser
+        md.inlinePatterns.register(del_tag, 'del', 75)
+        
